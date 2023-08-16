e53e60321e636788ccf0d2bed02d4a1a2e2e2d22
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-08-16 15:41:13 +0300

    done homework lesson1

---
+++ hw1/hw1.2/hw1.2.js
@@ -0,0 +1,5 @@
+let firstName = "Паньків";
+let middleName = "Богдан";
+let lastName = "Юрійович";
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
---
+++ hw1/hw1.4/hw1.4.js
@@ -0,0 +1,4 @@
+let name = prompt("write your name");
+let middleName = prompt("write your middle name");
+let age = prompt("write your age");
+console.log(name + " " + middleName + " " + age);
