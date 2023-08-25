1e20e50e65ed5ed11a2fc6c3cb629fd1c1849a17
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-08-25 16:36:45 +0300

    made some changes in 4 task function

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -3,15 +3,21 @@ function squareRectangle(a, b) {
     return a * b;
 }
 
+console.log(squareRectangle(2, 4));
+
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 function squareCircle(r) {
     return Math.PI * Math.pow(r, 2);
 }
 
+console.log(squareCircle(5));
+
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function squareСylinder(h, r) {
-    return 2 * Math.PI * r(h + r);
+function squareSelinder(h, r) {
+    return 2 * Math.PI * r * (h + r);
 }
+
+console.log(squareSelinder(15, 3));
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
 function printArray() {
@@ -20,11 +26,15 @@ function printArray() {
     }
 }
 
+console.log(printArray(2, 3, 5, 7));
+
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 function pharagraph(text) {
     document.write(`<p>${text}<p/>`);
 
 }
+
+console.log(pharagraph("hello world"));
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 function creatUlWithLi(text) {
@@ -35,6 +45,8 @@ function creatUlWithLi(text) {
     document.write(`<ul/>`);
 }
 
+console.log(creatUlWithLi("hello world"));
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 function creatUlWithLiAnotheOne(text, count = 3) {
     document.write(`<ul>`)
@@ -44,14 +56,18 @@ function creatUlWithLiAnotheOne(text, count = 3) {
     document.write(`<ul/>`)
 }
 
+console.log(creatUlWithLiAnotheOne("hello world"));
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 function listFromArr(arrays) {
+    document.write(`<ul>`);
     for (let array of arrays) {
-        document.write(`<ul>`);
         document.write(`<li>${array}</li>`);
-        document.write(`<ul/>`);
     }
+    document.write(`<ul/>`);
 }
+
+console.log(listFromArr(2, 3, "love", "dinamo"));
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 function arrayOfObject(arrays) {
@@ -61,6 +77,8 @@ function arrayOfObject(arrays) {
         document.write(array.age);
     }
 }
+
+console.log(arrayOfObject({id: 1, name: "bohdan", age: 27}, {id: 2, name: "oleg", age: 25}));
 
 // - створити функцію яка повертає найменьше число з масиву
 function minOfArray(arrays) {
@@ -73,14 +91,18 @@ function minOfArray(arrays) {
     return min;
 }
 
+console.log(minOfArray(2, 5, 3, 8, -8));
+
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 function sum(arr) {
-    let sum = a;
+    let sum = 0;
     for (let arrElement of arr) {
         sum += arrElement;
     }
     return sum;
 }
+
+console.log(sum(1, 2, 3, 4, 5));
 
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
@@ -93,8 +115,12 @@ function swap(arr, index1, index2) {
         }
     }
 }
+
+console.log(swap([2, 5, 6, 8, 7, 9, 4], 1, 3));
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 function exchange(sumUAH, currencyValues, exchangeCurrency) {
     return sumUAH / currencyValues.value;
 }
+
+console.log(exchange((10000, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}], 'USD')));
