4053e8bcb208043ab36f2c70ad6b53a900452619
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-09-01 17:27:38 +0300

    6 task made tasks with cards lesson6_array_functions

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -1,12 +1,12 @@
 // - Знайти та вивести довижину настипних стрінгових значень
-let h= 'hello world';
+let h = 'hello world';
 let l = 'lorem ipsum';
 let j = 'javascript is cool';
 console.log(h.length);
 console.log(l.length);
 console.log(j.length);
 // - Перевести до великого регістру наступні стрінгові значення
-let h1= 'hello world';
+let h1 = 'hello world';
 let l1 = 'lorem ipsum';
 let j1 = 'javascript is cool';
 console.log(h1.toUpperCase());
@@ -20,16 +20,16 @@ console.log(h2.toLowerCase());
 console.log(l2.toLowerCase());
 console.log(j2.toLowerCase());
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-let d  = ' dirty string   ';
+let d = ' dirty string   ';
 console.log(d.trim());
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
-     let str = 'Ревуть воли як ясла повні';
-     let srt1 = str.split(` `);
-     console.log(srt1);
+let str = 'Ревуть воли як ясла повні';
+let srt1 = str.split(` `);
+console.log(srt1);
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
-let arr = [10,8,-7,55,987,-1011,0,1050,0];
+let arr = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
 let arr1 = arr.map(value => value.toString());
 console.log(arr1);
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
@@ -44,7 +44,8 @@ function sortNums(nums, direction) {
         return nums.sort((a, b) => b - a);
     }
 }
-console.log(sortNums([2,5,6,8,4,6,4], 'ascending'));
+
+console.log(sortNums([2, 5, 6, 8, 4, 6, 4], 'ascending'));
 // ==========================
 // - є масив
 let coursesAndDurationArray = [
@@ -63,7 +64,7 @@ console.log(coursesAndDurationArray.filter(value => value.monthDuration > 5));
 console.log(coursesAndDurationArray.map(function (value, index) {
     value.id = index + 1;
     return value;
-} ));
+}));
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
 // - знайти піковий туз
@@ -71,7 +72,49 @@ console.log(coursesAndDurationArray.map(function (value, index) {
 // - всі червоні карти
 // - всі буби
 // - всі трефи від 9 та більше
-//
+const cardDeck = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+    {cardSuit: 'heart', value: '6', color: 'red'},
+    {cardSuit: 'heart', value: '7', color: 'red'},
+    {cardSuit: 'heart', value: '8', color: 'red'},
+    {cardSuit: 'heart', value: '9', color: 'red'},
+    {cardSuit: 'heart', value: '10', color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'},
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'}
+];
+console.log(cardDeck.filter(value => value.cardSuit === 'spade' && value.value === 'king'));
+console.log(cardDeck.filter(value => value.value === '6'));
+console.log(cardDeck.filter(value => value.color === 'red'));
+console.log(cardDeck.filter(value => value.cardSuit === 'diamond'));
+console.log(cardDeck.filter(value => value.cardSuit === 'clubs' && value.value !== '6' && value.value !== '7' && value.value !== '8'));
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
@@ -87,6 +130,19 @@ console.log(coursesAndDurationArray.map(function (value, index) {
 //     hearts:[],
 //     clubs:[]
 // }
+
+console.log(cardDeck.reduce(function (acumulator, value) {
+    if (value.cardSuit === 'spade') {
+        acumulator.spades.push(value);
+    } else if (value.cardSuit === 'diamond') {
+        acumulator.diamonds.push(value);
+    } else if (value.cardSuit === 'heart') {
+        acumulator.hearts.push(value);
+    } else if (value.cardSuit === 'clubs') {
+        acumulator.clubs.push(value);
+    }
+    return acumulator;
+}, {spades: [], diamonds: [], hearts: [], clubs: []}))
 // =========================
 //     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
 // --написати пошук всіх об'єктів, в який в modules є sass
